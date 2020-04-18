73a4f4e3b184848f8d5da98447e7db43f21e8e86
Giulio Bottari <giuliobottari@gmail.com>
2020-04-18 19:03:45 -0300

    Add bedroom scene

---
+++ BroGame/Assets/Scripts/GameController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System;
+using UnityEngine.SceneManagement;
+
+public class GameController : Singleton<GameController>
+{
+   private void Start() 
+   {
+     Debug.Log("Started");
+   } 
+}
