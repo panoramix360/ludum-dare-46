796ecffb7414a66c279a15acf6ba67b5184d962e
Lucas Reis <lucasreis3000@gmail.com>
2020-09-16 20:58:28 -0300

    flexing begin muscles

---
+++ BroGame/Assets/Scripts/Utils/GoButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GoButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        animator.SetBool("Flex", true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        animator.SetBool("Flex", false);
+    }
+}
