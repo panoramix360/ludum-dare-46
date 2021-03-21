82d3a0f277c31196d7a3af88d1aefa4caad7f8a4
Lucas Reis <lucasreis3000@gmail.com>
2021-03-21 13:44:55 -0300

    putting intro cinematics

--- BroGame/Assets/Scripts/Intro.cs
+++ BroGame/Assets/Scripts/Intro.cs
@@ -1,15 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 using UnityEngine.SceneManagement;
 
 public class Intro : MonoBehaviour
 {
+    [SerializeField] private AudioSource introMusic;
+    [SerializeField] private Canvas mainCanvas;
+    
+    private VideoPlayer introCinematics;
+
+    private void Start()
+    {
+        introCinematics = GetComponent<VideoPlayer>();
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            SceneManager.LoadScene("Bedroom");
+            mainCanvas.gameObject.SetActive(false);
+            introMusic.Stop();
+            introCinematics.Play();
+            introCinematics.loopPointReached += EndReached;
         }
+    }
+
+    private void EndReached(VideoPlayer vp)
+    {
+        SceneManager.LoadScene("Bedroom");
     }
 }
